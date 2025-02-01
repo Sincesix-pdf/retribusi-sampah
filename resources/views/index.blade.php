1f8cbfc8d155ba108c05345221f8c7f863ed424a
Sincesix-pdf <faizalyushrilimansyah@gmail.com>
2025-02-01 21:51:14 +0700

    tambah navbar dan sedikit di bagian body

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tes</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Retribusi Online</title>
+    <link rel="stylesheet" href="{{ asset(path: 'css/style.css') }}">
 </head>
+
 <body>
-    hai hai
-    <h1>Hello World!</h1>
+    <nav class="navbar">
+        <div class="left-content">
+            <img src="/gambar/GEH.png" alt="logo">
+            <div class="tittle">Retribusi Online</div>
+        </div>
+        <div class="right-content">
+            <a href="">
+                <span class="contact">Contact us</span>
+            </a>
+            <a href="" class="login-btn">Login</a>
+        </div>
+    </nav>
+
+    <header class="hero">
+        <div class="hero-text">
+            <h3>Selamat Datang di</h3>
+            <h1>Retribusi <br> Online Sistem</h1>
+            <p>Change For The Better</p>
+        </div>
+        <div class="hero-image">
+            <img src="/gambar/dashb.jpg" alt="Illustration">
+        </div>
+    </header>
+
+    <x-waves></x-waves>
+
 </body>
+
 </html>
